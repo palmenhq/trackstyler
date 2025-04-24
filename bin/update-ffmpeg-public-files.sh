71897f7523ef29dcb334d4e289cfec7c0a2036b1
Johan Palmfjord <johan@palmenhq.dev>
2025-04-24 18:59:34 +0200

    Add first prototype with ffmpeg 🥳

---
+++ bin/update-ffmpeg-public-files.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
+cp ./node_modules/@ffmpeg/core-mt/dist/esm/ffmpeg-core.js ./public/fmpc.js
+cp ./node_modules/@ffmpeg/core-mt/dist/esm/ffmpeg-core.worker.js ./public/fmpc.worker.js
+cp ./node_modules/@ffmpeg/core-mt/dist/esm/ffmpeg-core.wasm ./public/fmpc.wasm
+
+rm -rf ./public/fmp/
+mkdir ./public/fmp
+cp ./node_modules/@ffmpeg/ffmpeg/dist/esm/*.js ./public/fmp/
